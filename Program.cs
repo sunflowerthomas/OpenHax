e65930cf31e36a815edba4eddbd4f7bcb5384a99
sunflowerthomas <88040804+sunflowerthomas@users.noreply.github.com>
2022-09-20 16:35:33 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -32,10 +32,10 @@ namespace start
                 if (readline22 == "y" || readline22 == "Y")
                 {
                     Console.Clear();
-                    Console.Title = "OpenRec Tutorial";
-                    Console.WriteLine("In that case, welcome to OpenRec!");
-                    Console.WriteLine("OpenRec is server software that emulates the old servers of previous RecRoom versions.");
-                    Console.WriteLine("To use OpenRec, you'll need to have builds aswell!");
+                    Console.Title = "OpenHax Tutorial";
+                    Console.WriteLine("In that case, welcome to OpenHax!");
+                    Console.WriteLine("OpenHax is server software that emulates the old servers of previous RecRoom versions + Hacks.");
+                    Console.WriteLine("To use OpenHax, you'll need to have builds aswell!");
                     Console.WriteLine("To download builds, either go to the builds channel or use the links below: (these links are also available from the #builds channel)" + Environment.NewLine);
                     Console.WriteLine(new WebClient().DownloadString("https://raw.githubusercontent.com/recroom2016/OpenRec/master/Update/builds.txt"));
                     Console.WriteLine("Download a build and press any key to continue:");
@@ -63,15 +63,15 @@ namespace start
             }
 
         Start:
-            Console.Title = "OpenRec Startup Menu";
+            Console.Title = "OpenHax Startup Menu";
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("OpenRec - Open source Old RecRoom server software. (Version: " + appversion + ")");
-            Console.WriteLine("Made and provided by RecRoom 2016.");
-            Console.WriteLine("Download source code here: https://github.com/recroom2016/OpenRec");
+            Console.WriteLine("OpenHax - Open source Old RecRoom server software. (Version: " + appversion + ")");
+            Console.WriteLine("Made and provided by RecRoom 2016 - modded by sunflowerthomas.");
+            Console.WriteLine("Download source code here: https://github.com/sunflowerthomas/OpenHax");
             Console.WriteLine("Discord: https://discord.gg/daC8QUhnFP" + Environment.NewLine);
             if (!(new WebClient().DownloadString("https://raw.githubusercontent.com/recroom2016/OpenRec/master/Download/version.txt").Contains(appversion)))
             {
-                Console.WriteLine("This version of OpenRec is outdated. We recommend you install the latest version, OpenRec " + new WebClient().DownloadString("https://raw.githubusercontent.com/recroom2016/OpenRec/master/Download/version.txt"));
+                Console.WriteLine("This version of OpenHax is outdated. We recommend you install the latest version, OpenRec " + new WebClient().DownloadString("https://raw.githubusercontent.com/recroom2016/OpenRec/master/Download/version.txt"));
             }
             
             Console.WriteLine("//Custom Room Downloader has been moved to the settings tab!" + Environment.NewLine);
@@ -79,7 +79,7 @@ namespace start
             string readline = Console.ReadLine();
             if (readline == "1")
             {
-                Console.Title = "OpenRec Changelog";
+                Console.Title = "OpenHax Changelog";
                 Console.Clear();
                 Console.WriteLine(new WebClient().DownloadString("https://raw.githubusercontent.com/recroom2016/OpenRec/master/Download/changelog.txt"));
                 Console.WriteLine("Press any key to continue:");
@@ -93,7 +93,7 @@ namespace start
                 goto Settings;
 
                 Settings:
-                Console.Title = "OpenRec Settings Menu";
+                Console.Title = "OpenHax Settings Menu";
                 Console.WriteLine("(1) Private Rooms: " + File.ReadAllText("SaveData\\App\\privaterooms.txt") + Environment.NewLine + "(2) Custom Room Downloader " + Environment.NewLine + "(3) Reset SaveData" + Environment.NewLine + "(4) Go Back");
                 string readline4 = Console.ReadLine();
                 if (readline4 == "1")
@@ -112,7 +112,7 @@ namespace start
                 }
                 else if (readline4 == "2")
                 {
-                    Console.Title = "OpenRec Custom Room Downloader";
+                    Console.Title = "OpenHax Custom Room Downloader";
                     Console.Clear();
                     Console.WriteLine("Custom Room Downloader: This tool takes the room data of any room you type in and imports it into ^CustomRoom in September 27th 2018.");
                     Console.WriteLine("Please type in the name of the room you would like to download: (Case sensitive)");
@@ -177,7 +177,7 @@ namespace start
                 goto Profile;
 
             Profile:
-                Console.Title = "OpenRec Profile Menu";
+                Console.Title = "OpenHax Profile Menu";
                 Console.WriteLine("(1) Change Username" + Environment.NewLine + "(2) Change Profile Image" + Environment.NewLine + "(3) Change Level" + Environment.NewLine + "(4) Profile Downloader" + Environment.NewLine + "(5) Go Back");
                 string readline3 = Console.ReadLine();
                 if (readline3 == "1")
@@ -324,7 +324,7 @@ namespace start
             }
             if (readline == "4")
             {
-                Console.Title = "OpenRec Build Downloads";
+                Console.Title = "OpenHax Build Downloads";
                 Console.Clear();
                 Console.WriteLine("To download builds, either go to the builds channel or use the links below: (these links are also available from the #builds channel)" + Environment.NewLine);
                 Console.WriteLine(new WebClient().DownloadString("https://raw.githubusercontent.com/recroom2016/OpenRec/master/Update/builds.txt"));
@@ -335,12 +335,12 @@ namespace start
             }
             if (readline == "5")
             {
-                Console.Title = "OpenRec Version Select";
+                Console.Title = "OpenHax Version Select";
                 Console.WriteLine("Please select the version of RecRoom the server should host: (2016, 2017, 2018)");
                 string readline2 = Console.ReadLine();
                 if (readline2 == "2016")
                 {
-                    Console.Title = "OpenRec December 25th, 2016";
+                    Console.Title = "OpenHax December 25th, 2016";
                     version = "2016";
                     Console.Clear();
                     Console.WriteLine("Version Selected: December 25th, 2016.");
@@ -398,7 +398,7 @@ namespace start
                 Console.WriteLine(msg);
             }
         }
-        public static string msg = "//This is the server sending and recieving data from recroom." + Environment.NewLine + "//Ignore this if you don't know what this means." + Environment.NewLine + "//Please start up the build now.";
+        public static string msg = "//This is the server sending and recieving data from recroom." + Environment.NewLine + "//Ignore this if you don't know what this means." + Environment.NewLine + "//Please start up the build now." Environment.NewLine + "//Welcome to hax!!!";
         public static string version = "";
         public static string appversion = "0.6.9";
         public static bool bannedflag = false;
